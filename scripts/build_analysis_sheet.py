0f34894f8ac40977fc3c83e5d216d6f764d33968
Koichi Akabe <vbkaisetsu@gmail.com>
2014-03-10 13:29:00 +0900

    add documentations about creating sheet

--- scripts/build_analysis_sheet.py
+++ scripts/build_analysis_sheet.py
@@ -5,26 +5,26 @@ from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("orig", help="original sentences")
+parser.add_argument("src", help="original sentences")
 parser.add_argument("trg_refs", help="translation references")
 parser.add_argument("order_map", help="path to the map data to revert ordering")
 parser.add_argument("seed", help="seed file for building the evaluation sheet")
 
 args = parser.parse_args()
 
-org_file = open(args.orig, "r")
+src_file = open(args.src, "r")
 trg_file = open(args.trg_refs, "r")
 map_file = open(args.order_map, "w")
 seed_file = open(args.seed, "r")
 
 order_map = defaultdict(lambda : [])
 
-org_ref = []
+src_ref = []
 trg_ref = []
 
-for line in org_file:
-	org_ref.append(line.strip())
-org_file.close()
+for line in src_file:
+	src_ref.append(line.strip())
+src_file.close()
 
 for line in trg_file:
 	trg_ref.append(line.strip())
@@ -148,7 +148,7 @@ for i, ((sid, s), r) in enumerate(sorted(order_map.items(), key=lambda x: x[0][0
 			<td>Target MT:</td>
 			<td>%s</td>
 		</tr>
-	</table>""" %(sid, i, org_ref[sid], trg_ref[sid], s))
+	</table>""" %(sid, i, src_ref[sid], trg_ref[sid], s))
 	print(",".join(str(i) for i in r), file=map_file)
 
 print("""
